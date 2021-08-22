4dfefaba5ad24523636b52c8c27ed773eacef6c6
Hoang Cao Bao <62338248+hoangcaobao@users.noreply.github.com>
2021-08-22 22:02:34 +0700

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ def test(test_folder, save_path):
         img=cv2.imread(i, cv2.IMREAD_GRAYSCALE)
         cv2.imwrite(os.path.join(save_path,file_name), img)
         save_size=img.shape
-        img=cv2.resize(img, (256,256))
+        img=cv2.resize(img, (256,256), interpolation = cv2.INTER_NEAREST_EXACT)
         img=img/255
         img=np.array(img)
         img=np.expand_dims(img, axis=0)
@@ -25,7 +25,7 @@ def test(test_folder, save_path):
             for j in range(256):
                 if (predict[i][j]==0):
                     img[i][j]=0
-        img=cv2.resize(img, (save_size[1], save_size[0]))
+        img=cv2.resize(img, (save_size[1], save_size[0]), interpolation = cv2.INTER_NEAREST_EXACT))
         cv2.imwrite(os.path.join(save_path,file_name), img*255)
 
 test("data/test","result")
